96a3cfe27f64460d1c54f05d4ed02135c5b72211
Kirilov Nikita <nikita.kirilov.02@mail.ru>
2021-11-15 14:50:17 +0300

    create function conversion of km/h to m/s

--- task2.py
+++ task2.py
@@ -1,6 +1,7 @@
 # напишите функцию переводчик км/ч в м/с
-def translate(speed1):
+def translater(speed):
     speed1 = (speed * 1000) / 3600
+    print("Перевод завершён!\nОтвет : ", speed1, "м/с")
 if __name__ == '__main__':
     speed = int(input("Введите значение км/ч :  "))
-    print("Перевод завершён!\nОтвет : ", speed1)
+    translater(speed)
