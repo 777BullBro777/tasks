fc22407006c45fc54a0dde45e4c84f56927a50ef
Kirilov Nikita <nikita.kirilov.02@mail.ru>
2021-11-15 18:17:25 +0300

    create function comparison

---
+++ task3.py
@@ -0,0 +1,14 @@
+# напишите функцию поиска наименьшего из 3 введенных чисел
+def search(num1, num2, num3):
+    if num1 < num2 and num1 < num3:
+        print(num1, "Наименьшее число!")
+    elif num2 < num1 and num2 < num3:
+        print(num1, "Наименьшее число!")
+    else:
+        print(num3, "Наименьшее число!")
+if __name__ == '__main__':
+    num1 = int(input("Введите целое число : "))
+    num2 = int(input("Введите целое число : "))
+    num3 = int(input("Введите целое число : "))
+    search(num1, num2, num3)
+
