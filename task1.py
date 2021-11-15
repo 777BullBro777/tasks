576b269049da144dee40827daf6880794be58424
Kirilov Nikita <nikita.kirilov.02@mail.ru>
2021-11-15 12:10:06 +0300

    add 7 paragraph and function conversion

--- task1.py
+++ task1.py
@@ -11,7 +11,70 @@ def factorial(number):
         return 1
     else:
         return number * factorial(number - 1)
-
+def degreed(number, degree):
+    print("Ответ : ", number ** degree)
+def module(number):
+    if number > 0:
+        print("| ", number, " | =  ", number)
+    elif number == 0:
+        print("| ", number, " | =  0")
+    elif number < 0:
+        print("| ", number, " | =  ", number * (-1))
+def log(number, base):
+    math.log(number, base)
+def procent(array):
+    number = None
+    kolvo = 0
+    array = []
+    while number != 0:
+        number = float(input("Введите число от 1 до 100 :  "))
+        kolvo = +1
+        if number == 0:
+            array.pop(0)
+            kolvo = -1
+            result1 = sum(array)
+            result2 = 100 / (kolvo * 100)
+            print("Ответ : ", result1 * result2, "%")
+            break
+        if number >= 101:
+            print("Ошибка")
+            exit()
+        elif number <= 100:
+            array.append(number)
+            print(array)
+def isfloat(num1, num2):
+    try:
+        num1(float)
+        num2(float)
+        return True
+    except ValueError:
+        print("Ошибка!", ValueError)
+        return False
+def simple_operation(x):
+    num1 = float(input("Введите первое  число : "))
+    zna4 = input(str('Введите знак : '))
+    num2 = float(input('Введите второе число : '))
+    if zna4 == str("+"):
+        x = num1 + num2
+        print(x)
+    elif zna4 == str("-"):
+        x = num1 - num2
+        print(x)
+    elif zna4 == str("*"):
+        x = num1 * num2
+        print(x)
+    elif zna4 == str("/"):
+        try:
+            if zna4 == str("/"):
+                x = num1 / num2
+                num1 = float(num1)
+                num2 = float(num2)
+        except ZeroDivisionError:
+            print(ZeroDivisionError, "Деление на ноль!")
+            x = 0
+            print(x)
+        else:
+            print(num1 / num2)
 if __name__ == '__main__':
     menu = int(input("Введите операцию :\n1)расчёт факториала\n2)возведение в степень"
                         "\n3)модуль\n4)логарифм\n5)расчёт процентоного соотношения"
@@ -19,68 +82,29 @@ if __name__ == '__main__':
     if menu == 1:
         number = int(input("Введите число :  "))
         factorial(number)
-        print("Ответ :  ", factorial(number))
+        print("Ответ : ", factorial(number))
     elif menu == 2:
         number = int(input("Введите число для возведения его в степень :  "))
         degree = int(input("Введите степень возведения :  "))
-        print("Ответ : ", number ** degree)
+        degreed(number, degree)
     elif menu == 3:
         number = int(input("Введите число :  "))
-        if number > 0:
-            print("| ", number, " | =  ", number)
-        elif number == 0:
-            print("| ", number, " | =  0")
-        elif number < 0:
-            print("| ", number, " | =  ", number * (-1))
+        module(number)
     elif menu == 4:
         number = int(input("Введите число :  "))
         base = int(input("Введите базовое значение :  "))
-        print("Ответ : ", math.log(number, base))
+        print("Ответ : ", log(number, base))
     elif menu == 5:
         number = None
         kolvo = 0
         array = []
-        while number != 0:
-            number = float(input("Введите число от 1 до 100 :  "))
-            kolvo = +1
-            if number == 0:
-                array.pop(kolvo)
-                kolvo = -1
-                result1 = sum(array)
-                result2 = (100 / (kolvo * 100))
-                print("Ответ : ", result1 * result2, "%")
-            if number >= 101:
-                print("Ошибка")
-                exit()
-            elif number <= 100:
-                array.append(number)
-                print(array)
-    if menu == 6:
-        num1 = float(input("Введите первое  число : "))
-        zna4 = input(str('Введите знак : '))
-        num2 = float(input('Введите второе число : '))
-        if zna4 == str("+"):
-            x = num1 + num2
-            print(x)
-        elif zna4 == str("-"):
-            x = num1 - num2
-            print(x)
-        elif zna4 == str("*"):
-            x = num1 * num2
-            print(x)
-        try:
-            if zna4 == str("/"):
-                x = num1 / num2
-            num1 = float(num1)
-            num2 = float(num2)
-        except ZeroDivisionError:
-            print(ZeroDivisionError, "Деление на ноль!")
-            x = 0
-            print(x)
-        else:
-            print(num1 / num2)
+        procent(array)
+    elif menu == 6:
+        x = None
+        simple_operation(x)
     elif menu == 7:
+        print("Выход из программы!")
         exit()
-    else:
-        print("Ошибка введённого значения! Выход из программы!")
-        exit()
+else:
+    print("Ошибка введённого значения! Выход из программы!")
+    exit()
--- task2.py
+++ task2.py
@@ -1,3 +1,6 @@
 # напишите функцию переводчик км/ч в м/с
-speed = int(input("Введите значение км/ч :  "))
-
+def translate(speed1):
+    speed1 = (speed * 1000) / 3600
+if __name__ == '__main__':
+    speed = int(input("Введите значение км/ч :  "))
+    print("Перевод завершён!\nОтвет : ", speed1)
